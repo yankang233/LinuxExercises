11758e4636faedfc5ad1a30ee232aaaebe7ad937
Zekai Xu <zxu653@wisc.edu>
2024-10-17 13:52:02 -0500

    Add five_dirs.sh script for directory and file creation

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+
+
+
+
+mkdir -p five
+
+
+for i in {1..5}; do
+
+  mkdir -p five/dir$i
+
+
+  for j in {1..4}; do
+
+    for k in $(seq 1 $j); do
+      echo $j >> five/dir$i/file$j
+    done
+  done
+done
+
