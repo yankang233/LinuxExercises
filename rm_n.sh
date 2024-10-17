19c6a8777900a2329c03a6988f707e4819ecff5f
JirenLu <lujiren2021@hotmail.com>
2024-10-17 14:05:26 -0500

    revise rm_n.sh

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <dir> <n>" 1>&2
+  exit 1
+fi
+
+DIR=$1
+SIZE=$2
+
+if [ ! -d "$DIR" ]; then
+  echo "Error: Directory $DIR does not exist." 1>&2
+  exit 1
+fi
+
+find "$DIR" -type f -size +"$SIZE"c -exec rm -f {} \;
