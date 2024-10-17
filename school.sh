92c061bc0faaaa973ef3b07081b406c853878d51
JirenLu <lujiren2021@hotmail.com>
2024-10-17 13:43:37 -0500

    Add school.sh script to calculate average TotalAssessedValue for MADISON SCHOOLS

--- school.sh
+++ school.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+cat Property_Tax_Roll.csv | \
+grep "MADISON SCHOOLS" | \
+cut -d',' -f7 | \
+awk '{sum += $1; count++} END {if (count > 0) print "Average TotalAssessedValue:", sum / count}'
